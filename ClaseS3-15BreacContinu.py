94a972f7df5945cfeed19ab56448f85500e6268a
Jorge Zabala Medrano <67214281+joluzam@users.noreply.github.com>
2022-06-14 01:05:32 +0000

    Renombrado de archivos

--- ClaseS3-15BreacContinu.py
+++ ClaseS3-15BreacContinu.py
@@ -1,5 +1,5 @@
-#Secuencia Fibonacci
 """
+#Secuencia Fibonacci
 Cada nuevo término en la sucesión de Fibonacci se genera sumando los dos términos anteriores. Al comenzar con 1 y 2, los primeros 10 términos serán:
 
 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
