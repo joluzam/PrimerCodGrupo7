d8f8622351ed4f717affa53f65eb6b84c578ac8c
Jorge Zabala Medrano <joluzam@gmail.com>
2022-06-15 19:04:45 -0500

    Ejercicios de ciclo for

---
+++ Ciclo for.py
@@ -0,0 +1,17 @@
+diccionario = {"a": 10, "b": 20, "c": 30}
+
+for x in diccionario:
+         print(x, "es la clave")
+for y in diccionario.values():
+        print(y, "es el valor")
+         
+for x, y in diccionario.items():
+        print(x, y, "clave : Valor")
+
+for y in diccionario.items():
+        print(y, "elemento con su clave y valor")        
+
+ 
+
+
+
